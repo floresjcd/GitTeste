388cbb8ab917af6642f423c591df9547cbb968fd
José Carlos Flores <30850074+floresjcd@users.noreply.github.com>
2026-02-27 08:15:42 -0300

    Update success message in Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("------------------------------------");
-        System.out.println("✅ Java executado com sucesso no GitHub!");
+        System.out.println("✅ Java executado com sucesso no GitHub by Flores!");
         System.out.println("Hora atual do sistema: " + java.time.LocalDateTime.now());
         System.out.println("------------------------------------");
     }
